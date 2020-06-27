e9892ffdb98309634d4dde2b9646abff63c2bdea
Signior-X <b19188@students.iitmandi.ac.in>
2020-06-27 10:51:42 +0530

    Initiate

--- assets/scripts.js
+++ assets/scripts.js
@@ -20,4 +20,32 @@ $(function () {
       listUrls.addClass('is--visible');
     }
   });
+
+  // <!-- Script for code copying -->
+  $('pre').hover(function() {
+    console.log("Hello You just hovered this pre ");
+
+    // console.log(this);
+    // console.log($(this));
+
+    if(this.classList.contains('is-hovered')){
+      this.classList.remove('is-hovered');
+      console.log("Mouse leaved outside ");
+
+      // Remove the copy button from the parent
+    } else {
+      console.log("Mouse entered inside ");
+      this.classList.add('is-hovered');
+      console.log(this.textContent);
+
+      // This is creating a problem for right now think of something else
+      // Add the content one thing and copy button to the parent
+      // console.log(this.parentNode);
+      // var toBeAdded = '<div id="copy-code-button">' 
+      // + '</div>';
+
+      // this.parentNode.innerHTML += toBeAdded;
+      console.log("check now!");
+    }
+  });
 })
